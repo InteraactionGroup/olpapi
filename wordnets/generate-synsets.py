53dd041e1586fa62c7e1d34f584ded498994bb6b
Linux User <ath@skydesk.my.domain>
2021-07-05 12:00:35 +0200

    feat: OEMW parsing is now done via a python library

--- wordnets/generate-synsets.py
+++ wordnets/generate-synsets.py
@@ -1,28 +1,20 @@
 import json
+import tabfile
 
 # using files from http://compling.hss.ntu.edu.sg/omw/summx.html
 
-def conv(lang):
-	tab_file = open('wikt/wn-wikt-' + lang + '.tab')
-	lines = tab_file.read().split('\n')
-	tab_file.close()
+def callback(output, parts):
+	if len(parts) != 3: return
+	synset, word = parts[0].replace('-', ''), parts[2]
+	if word in output: output[word].append(synset)
+	else: output[word] = [synset]
 
+def conv(lang, path):
 	output = {}
+	tabfile.parse(path, callback, output)
 
-	for line in lines:
-		parts = line.split('\t')
-		if len(parts) != 3: continue
-		synset, word = parts[0].replace('-', ''), parts[2]
-		if word in output: output[word].append(synset)
-		else: output[word] = [synset]
-
-	json_file = open('json/' + lang + '.json', 'w')
+	json_file = open('synsets.json', 'w')
 	json.dump(output, json_file)
 	json_file.close()
 
-tab_file = open('wikt.txt')
-lines = tab_file.read().split('\n')
-tab_file.close()
-
-for line in lines: conv(line)
-exit()
+conv('fra', 'wikt.tab')
